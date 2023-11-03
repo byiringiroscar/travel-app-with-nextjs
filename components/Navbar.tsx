3a15f0114ccb26eaf0889360aa853484677e3361
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-03 16:04:17 +0200

    Add mapped links to nav from constants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import logo from '../public/hilink-logo.svg';
+import { NAV_LINKS } from "@/constants";
 
 
 const Navbar = () => {
@@ -10,6 +11,9 @@ const Navbar = () => {
             <Image alt="logo" src={logo} width={74} height={29} />
         </Link>
         <ul className="hidden h-full gap-12 lg:flex">
+            {NAV_LINKS.map((link) => (
+                <Link href={link.href} key={link.key}>{link.label}</Link>
+            ))}
 
         </ul>
 
