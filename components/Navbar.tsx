417e1b2944456858d3c88538ee70f063c7104f16
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-03 15:59:47 +0200

    Add constants to handle many lines

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,17 @@
+import Image from "next/image"
 import Link from "next/link"
+import logo from '../public/hilink-logo.svg';
 
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
         <Link href='/'>
-            
+            <Image alt="logo" src={logo} width={74} height={29} />
         </Link>
+        <ul className="hidden h-full gap-12 lg:flex">
+
+        </ul>
 
     </nav>
   )
