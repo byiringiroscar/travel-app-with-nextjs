9be7e19ffc160f359b322a5778eb2916a304a165
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-05 01:12:35 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <Image alt='camp' src='/camp.svg' width={50} height={50}
                 className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]'
             />
-            <h1 className='bold-52 lg:bold-88 lg:w-1/2'>Putuk Truno Camp Area</h1>
+            <h1 className='bold-52 lg:bold-88'>Putuk Truno Camp Area</h1>
             <p className='regular-16 mt-6 text-gray-30 xl:max-w-[520px]'>We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app</p>
             <div className='my-11 flex flex-wrap gap-5'>
                 <div className='flex items-center gap-2'>
