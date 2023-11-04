bdff1fefa04134f91786e795a28914dc1c796197
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-05 00:02:10 +0200

    Add stars

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,6 +12,13 @@ const Hero = () => {
             />
             <h1 className='bold-52 lg:bold-88 lg:w-1/2'>Putuk Truno Camp Area</h1>
             <p className='regular-16 mt-6 text-gray-30 xl:max-w-[520px]'>We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app</p>
+            <div className='my-11 flex flex-wrap gap-5'>
+                <div className='flex items-center gap-2'>
+                    {Array(5).fill(1).map((_, index) => (
+                        <Image key={index} alt='star' src='/star.svg' width={24} height={24} />
+                    ))}
+                </div>
+            </div>
 
         </div>
         
