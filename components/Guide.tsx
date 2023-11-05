ef323a22883ccf246c7b165e4cf724d7f0e41c07
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-05 22:19:28 +0200

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Guide = () => {
   return (
-    <div>Guide</div>
+    <section className='flexCenter flex-col'>
+      <div className='border-2 border-red-500 padding-container max-container w-full pb-24'>
+        <Image src='/camp.svg' alt='camp' width={24} height={24} />
+        <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>We are here for you</p>
+      </div>
+
+
+    </section>
   )
 }
 
