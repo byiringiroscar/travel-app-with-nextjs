9b8b4d3a6040d168868e3b1f3f00479b95e415e7
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-05 22:10:58 +0200

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -69,7 +69,7 @@ const Camp = () => {
             <strong>Feeling Lost</strong> And not knowing the way ?
           </h2>
           <p className="regular-14 xl:regular-16 mt-5 text-white">Starting from the anxiety of the climbers when visiting a new climbing location, the possibility of getting lost is very large. That's why we are here for those of you who want to start an adventure</p>
-
+          <Image src='/quote.svg' alt="camp-2" width={186} height={219} className="camp-quote" />
         </div>
       </div>
 
