f56de7d41f077a0068d8e4cc339d585ff3fd1efb
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-05 23:33:21 +0200

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -15,7 +15,29 @@ const Guide = () => {
       </div>
       <div className='flexCenter max-container relative w-full border-2 border-blue-500'>
         <Image src='/boat.png' alt='boar' width={1440} height={580} className='w-full object-cover object-center 2xl:rounded-5xl' />
-        <div></div>
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image 
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+          <div className="flexBetween flex-col">
+            <div className='flex w-full flex-col'>
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Aguas Calientes</p>
+            </div>
+
+            <div className='flex w-full flex-col'>
+              <p className="regular-16 text-gray-20">Start track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</h4>
+            </div>
+          </div>
+        </div>
       </div>
 
 
