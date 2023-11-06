25e9126f6ddfab181d22c66592a51d1ba58bd7fd
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-07 01:06:42 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,39 @@
+import { FOOTER_LINKS } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flexCenter mb-24'>
+      <div className='padding-container max-container flex w-full flex-col gap-14'>
+        <div className='flex flex-col items-start justify-center gap-[10%] md:flex-row'>
+          <Link href='/' className='mb-10'>
+            <Image src='hilink-logo.svg' alt='logo' width={74} height={29} />
+          </Link>
+          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+            {FOOTER_LINKS.map((columns) => (
+              <FooterColumn title={columns.title}>
+                asaasas
+              </FooterColumn>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+type FooterColumnProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const FooterColumn = ({title, children}: FooterColumnProps) => {
+  return (
+    <div className='flex flex-col gap-4'>
+      <h4>{title}</h4>
+    </div>
   )
 }
 
